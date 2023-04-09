181125c852e8c1171921b1da3d82e2a7cc6e0287
khanhmeo0901 <khanhvn09012000@gmail.com>
2023-04-09 11:02:51 +0700

    Java Core

--- src/main/java/com/example/learnjavacore/Exception/Exception.java
+++ src/main/java/com/example/learnjavacore/Exception/Exception.java
@@ -12,6 +12,22 @@ public class Exception {
                 bởi lập trình viên
             + Error : Nó không giống với các Exception , nhưng vấn đề xảy ra vượt quá tầm kiểm
                 soát của lập trình viên  hay người dùng.
+         - Xử lí ngoại lệ (Exception Handling) : là một kĩ thuật xử lí các Runtime Error như
+            ClassNotFound, IO, SQL, Remote,...
+         - Các cấp bậc trong Exception :
+            + Tất cả các lớp Exception đều là lớp con của lớp java.lang.Exception
+            + Lớp Exception là lớp con của lớp Throwable
+            + Một loại Exception khác là Error cũng là lớp con của lớp Throwable.
+            + Error không thường được đặt bẫy bởi các chương trình Java.
+            + Error thường được tạo ra để thể hiện lỗi trong môi trường Runtime.
+         - Các tình huống phổ biến mà Exception có thể xảy ra:
+            + Với ArithmaticException : Xảy ra nếu chia cho 0
+            + Với NullPointerException : Nếu bạn có giá trị null trong bất cứ biê nào
+                thì việc thực hiện bất cứ hoạt động nào bởi biến này làm xuất hiện kiểu exception này.
+            + Với NumberFormatException : Việc định dạng sai bất cứ giá trị nào, có thể gây ra loại ngoại lệ
+                này.
+            + Với ArrayIndexOutOfBoundsException : Nếu bạn đang chèn bất cứ giá trị nào trong một chỉ mục sai
+                thì sẽ làm xuất hiện kiểu exception này.
      */
     public static void main(String[] args) {
 
